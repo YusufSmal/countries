1139f51b04fecd2f916e9b520446dd911942a887
Yusuf Smal <timtim.smal@gmail.com>
2023-11-28 15:22:52 +0200

    updated routing for new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CountryListComponent } from './country-list/country-list.component';
 import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
 
 const routes: Routes = [
   { path: 'country-list', component: CountryListComponent },
+  { path: 'country-details/:id', component: CountryDetailsComponent },
   { path: 'favorites', component: FavoritesPageComponent },
 
-  {path: '**', component: CountryListComponent},
+  // {path: '**', component: CountryListComponent},
   {path: '', redirectTo: 'country-list', pathMatch: 'full'},
 ];
 
