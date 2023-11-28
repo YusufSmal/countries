13b57ac24c8293acb8c212513eaf9c2594b9e446
Yusuf Smal <timtim.smal@gmail.com>
2023-11-28 11:34:54 +0200

    creating the fancionality for the modal popup

--- src/app/country-modal/country-modal.component.ts
+++ src/app/country-modal/country-modal.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-country-modal',
   templateUrl: './country-modal.component.html',
-  styleUrl: './country-modal.component.scss'
+  styleUrls: ['./country-modal.component.scss'],
 })
-export class CountryModalComponent {
+export class CountryModalComponent implements OnInit {
+  favoriteCountries: any[] = [];
 
+  private favoriteUpdateSubject = new Subject<any>();
+  favoriteUpdate$ = this.favoriteUpdateSubject.asObservable();
+
+  constructor(
+    public dialogRef: MatDialogRef<CountryModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { country: any }
+  ) {}
+
+  ngOnInit(): void {
+    const storedFavorites = localStorage.getItem('favoriteCountries');
+    this.favoriteCountries = storedFavorites ? JSON.parse(storedFavorites) : [];
+  }
+
+  toggleFavorite(country: any): void {
+    const index = this.favoriteCountries.findIndex( 
+      (fav) => fav.name === country.name.common );
+
+    if (index !== -1) {
+      this.favoriteCountries.splice(index, 1);
+    } else {
+      this.favoriteCountries.push({
+        name: country.name.common,
+      });
+    }
+
+    localStorage.setItem(
+      'favoriteCountries',
+      JSON.stringify(this.favoriteCountries)
+    );
+  }
+
+  closeModal(result: any = null): void {
+    this.favoriteUpdateSubject.next({ updatedCountry: this.data.country });
+    this.dialogRef.close(result);
+  }
+
+  isFavorite(country: any): boolean {
+    return this.favoriteCountries.some(
+      (fav) => fav.name === country.name.common
+    );
+  }
 }
